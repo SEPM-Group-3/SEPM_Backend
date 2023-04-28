ca6a08e1ad4fc58059b343fe105033d9a06a4518
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-04-28 07:19:13 +0530

    :boom: dump code

--- build/api/index.js
+++ build/api/index.js
@@ -4,13 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const ses_1 = __importDefault(require("./ses"));
-const s3_1 = __importDefault(require("./s3"));
 const index_1 = __importDefault(require("./election/index"));
 exports.default = () => {
     const app = (0, express_1.Router)();
-    app.use('/aws-SES', ses_1.default);
-    app.use('/aws-S3', s3_1.default);
     app.use('/election', index_1.default);
     return app;
 };
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import sesRouter from './ses';
-import s3Router from './s3';
 import ElectionRouter from './election/index';
 
 export default (): Router => {
   const app = Router();
-
-  app.use('/aws-SES', sesRouter);
-  app.use('/aws-S3', s3Router);
   app.use('/election', ElectionRouter);
   return app;
 };
